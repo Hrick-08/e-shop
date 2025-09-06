52ec94ca3fa4df2c917836a2dbcd62e4c57a7109
Hritabrata Das <119574039+Hrick-08@users.noreply.github.com>
2025-09-06 19:15:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production" || true) {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 } else {
